93d779ab322a71e6fe68dbe7b1ed72e0cbf2fcfe
MadeInChina <slamhan1987@gmail.com>
2013-07-04 16:27:09 +0800

    reformatted

--- demo-App/myapp-core/src/test/java/com/madeinchina/myapp/ComponentStartupTest.java
+++ demo-App/myapp-core/src/test/java/com/madeinchina/myapp/ComponentStartupTest.java
@@ -1,3 +1,4 @@
+
 package com.madeinchina.myapp;
 
 import java.io.File;
@@ -10,33 +11,35 @@ import atg.nucleus.Nucleus;
 import atg.nucleus.NucleusTestUtils;
 
 public class ComponentStartupTest extends TestCase {
-	public void testComponentStartup() throws Exception {
-		File configpath = NucleusTestUtils.getConfigpath(this.getClass(),
-				"config");
-		// Put test .properties file in configpath path
-		Properties props = new Properties();
-		File propFile = NucleusTestUtils.createProperties("test/resources/config/atg/modules/ToBeTested",
-				configpath, "com.madeinchina.myapp.ToBeTested", props);
-		propFile.deleteOnExit();
-		List initial = new ArrayList();
-		initial.add("/test/resources/config/atg/modules/ToBeTested");
-		NucleusTestUtils.createInitial(configpath, initial);
-		Nucleus n = NucleusTestUtils.startNucleus(configpath);
-		ToBeTested testComponent = null;
-		try {
-			testComponent = (ToBeTested) n.resolveName("/test/resources/config/atg/modules/ToBeTested");
-			assertNotNull("Could not resolve test componet", testComponent);
-			assertTrue("Test component did not start up cleanly.",
-					testComponent.mCleanStart);
+    public void testComponentStartup() throws Exception {
+        File configpath = NucleusTestUtils.getConfigpath(this.getClass(),
+                "config");
+        // Put test .properties file in configpath path
+        Properties props = new Properties();
+        File propFile = NucleusTestUtils.createProperties(
+                "test/resources/config/atg/modules/ToBeTested",
+                configpath, "com.madeinchina.myapp.ToBeTested", props);
+        propFile.deleteOnExit();
+        List initial = new ArrayList();
+        initial.add("/test/resources/config/atg/modules/ToBeTested");
+        NucleusTestUtils.createInitial(configpath, initial);
+        Nucleus n = NucleusTestUtils.startNucleus(configpath);
+        ToBeTested testComponent = null;
+        try {
+            testComponent = (ToBeTested) n
+                    .resolveName("/test/resources/config/atg/modules/ToBeTested");
+            assertNotNull("Could not resolve test componet", testComponent);
+            assertTrue("Test component did not start up cleanly.",
+                    testComponent.mCleanStart);
 
-		} finally {
-			n.stopService();
-			assertNotNull(testComponent);
-			assertFalse("Test component did not shut down cleanly.",
-					testComponent.mCleanStart);
-			testComponent = null;
-		}
+        } finally {
+            n.stopService();
+            assertNotNull(testComponent);
+            assertFalse("Test component did not shut down cleanly.",
+                    testComponent.mCleanStart);
+            testComponent = null;
+        }
 
-	}
+    }
 
 }
