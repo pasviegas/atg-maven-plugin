b2dfbc9e09f193dc785da8d638a357eb767cd82d
MadeInChina <slamhan1987@gmail.com>
2013-07-04 16:25:26 +0800

    fix test can't work issue.

--- atg-maven-plugin/src/test/java/org/codehaus/mojo/atg/AtgAssemblerMojoTest.java
+++ atg-maven-plugin/src/test/java/org/codehaus/mojo/atg/AtgAssemblerMojoTest.java
@@ -1,3 +1,4 @@
+
 package org.codehaus.mojo.atg;
 
 import java.io.File;
@@ -5,31 +6,35 @@ import java.io.File;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 
 public class AtgAssemblerMojoTest extends AbstractMojoTestCase {
-	/** {@inheritDoc} */
-	protected void setUp() throws Exception {
-		// required
-		super.setUp();
+    /** {@inheritDoc} */
+    protected void setUp() throws Exception {
+        // required
+        super.setUp();
 
-	}
+    }
 
-	/** {@inheritDoc} */
-	protected void tearDown() throws Exception {
-		// required
-		super.tearDown();
+    /** {@inheritDoc} */
+    protected void tearDown() throws Exception {
+        // required
+        super.tearDown();
 
-	}
+    }
 
-	/**
-	 * @throws Exception
-	 *             if any
-	 */
-	public void test_maven_project_should_not_return_null() throws Exception {
-		File pom = getTestFile("src/test/resources/unit/pom.xml");
-		assertNotNull(pom);
-		assertTrue(pom.exists());
+    /**
+     * @throws Exception if any
+     */
+    public void test_should_not_return_null_when_lookup_goal_atg_assemble() throws Exception {
+        File pom = getTestFile("src/test/resources/unit/pom.xml");
+        AtgAssemblerMojo myMojo = (AtgAssemblerMojo) lookupMojo("atg-assemble", pom);
+        assertNotNull(myMojo);
+    }
 
-		DarMojo myMojo = (DarMojo) lookupMojo("run", pom);
-
-		assertNotNull(myMojo);
-	}
+    /**
+     * @throws Exception if any
+     */
+    public void test_should_return_default_atg_home_when_get_atg_home() throws Exception {
+        File pom = getTestFile("src/test/resources/unit/pom.xml");
+        AtgAssemblerMojo myMojo = (AtgAssemblerMojo) lookupMojo("atg-assemble", pom);
+        assertEquals("D:\\GCWorkbench\\ATG", myMojo.atgHome.getPath());
+    }
 }
